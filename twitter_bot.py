4cdc3545a31b1bf79b88405030619c91a0676b2e
Balveer Singh <balveersinghyt@gmail.com>
2021-05-19 21:44:53 +0530

    Now Tag person

--- twitter_bot.py
+++ twitter_bot.py
@@ -20,15 +20,18 @@ def OAuth():
 auth = OAuth()
 api = tweepy.API(auth)
 
+user_name = 'subham_shweta' # Change 'subham_shweta' to any other twitter username
+
 while True:
-    user = api.get_user('subham_shweta') # Change 'subham_shweta' to any other twitter username
+    user = api.get_user(user_name)  
     copy = user.status.text
     try:
-        api.update_status(copy)         # Post copied tweet
-        print('Paste')
+        api.update_status('"%s" ~ @%s'%(copy,user_name))         # Post copied tweet
+        print('Pasted')
         time.sleep(5)
 
     except Exception as e:
         print(e)
         time.sleep(10)
         continue
+
