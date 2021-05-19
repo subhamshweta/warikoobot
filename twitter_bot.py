ce3790d8267fabeaf1600112701659089adf03ca
subhamshweta <84373980+subhamshweta@users.noreply.github.com>
2021-05-19 20:57:42 +0530

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -18,10 +18,11 @@ def OAuth():
         return e
 
 auth = OAuth()
-api = tweepy.API(auth)
+api = tweepy.API(auth + mention_timeline)
+mention in reversed(mention):
 
 while True:
-    user = api.get_user('subham_shweta')  # Change 'subham_shweta' to any other twitter username
+    user = api.get_user('subham_shweta'+ mention.user.screen_name) # Change 'subham_shweta' to any other twitter username
     copy = user.status.text
     try:
         api.update_status(copy)         # Post copied tweet
@@ -30,5 +31,6 @@ while True:
 
     except Exception as e:
         print(e)
+        print(mention.id) + "_" + mention.full_text)
         time.sleep(10)
         continue
